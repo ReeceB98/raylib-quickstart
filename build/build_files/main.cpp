b17764763f4c2efdefdf3934d806ce6b97ca70f3
ReeceB98 <reecebellchambers98@gmail.com>
2025-06-07 18:50:27 +0100

    Created a scene manager
    Scene manager is shown by cycling through the examples by using the frames as the timer.

--- build/build_files/main.cpp
+++ build/build_files/main.cpp
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+typedef enum gameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } gameScreen;
+
 int main()
 {
 	const int screenWidth = 800;
@@ -7,15 +9,65 @@ int main()
 
 	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
+	gameScreen currentScreen = LOGO; // Current game screen
+
 	SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+	int frameCounter = 0; // Frame counter to switch screens
 
 	while (!WindowShouldClose()) // Detect window close button or ESC key
 	{
 		BeginDrawing();
 
-		ClearBackground(BLACK);
+		frameCounter++; // Increment frame counter
 
-		DrawText("Congratulations! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		switch (currentScreen)
+		{
+		case LOGO:
+			ClearBackground(BLACK);
+			DrawText("LOGO SCREEN", 190, 200, 20, LIGHTGRAY);
+
+			if (frameCounter > 120)
+			{
+				currentScreen = TITLE;
+			}
+			break;
+
+		case TITLE:
+			ClearBackground(GREEN);
+			DrawText("TITLE SCREEN", 190, 200, 20, LIGHTGRAY);
+
+			if (frameCounter > 240)
+			{
+				currentScreen = GAMEPLAY;
+			}
+			break;
+
+		case GAMEPLAY:
+			ClearBackground(ORANGE);
+			DrawText("GAMEPLAY SCREEN", 190, 200, 20, LIGHTGRAY);
+
+			if (frameCounter > 360)
+			{
+				currentScreen = ENDING;
+			}
+			break;
+
+		case ENDING:
+			ClearBackground(RED);
+			DrawText("ENDING SCREEN", 190, 200, 20, LIGHTGRAY);
+
+			if (frameCounter > 480)
+			{
+				currentScreen = LOGO; // Loop back to the logo screen
+				frameCounter = 0; // Reset frame counter
+			}
+			break;
+
+		default:
+			break;
+		}
+
 
 		EndDrawing();
 	}
